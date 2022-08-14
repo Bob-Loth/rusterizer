91a893e40ceaeadc8e6631fae94493e56616c2cb
Bob-Loth <36461371+Bob-Loth@users.noreply.github.com>
2022-08-13 22:04:29 -0700

    Comment-outline the program, add Cargo.lock to version control.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    //command arguments: meshfile imagefile imagewidth imageheight mode
+
+    //extract information from the mesh file, resize if necessary
+    //store triangle's indices and vertex positions into packed data structures.
+
+    //maintain a z buffer, a 2d structure to store depth information per pixel.
+
+    //the actual rasterization operation.
+    //Points(x,y,z)
+    //for each triangle(p1, p2, p3):
+        //compute bounding box, as pair<Point, Point> or similar.
+        //pre-compute static barycentric coordinates factor.
+        //for each pixel in the bounding box:
+            //compute barycentric coordinates alpha, beta, gamma.
+            //if all (alpha, beta, gamma) >= 0 and <= 1:
+                //color according to mode.
+
+    //write out the data to an output file.
 }

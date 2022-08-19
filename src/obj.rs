7a337ebae0457e6171cb0c519d787df2457ca567
Bob-Loth <36461371+Bob-Loth@users.noreply.github.com>
2022-08-18 20:51:03 -0700

    Remove debug print obj, removed unused criterion dev-dependency.

--- src/obj.rs
+++ src/obj.rs
@@ -68,44 +68,6 @@ impl Transform {
     }
 }
 
-fn print_obj(obj: &[Model]) {
-    println!("number of models: {}", obj.len());
-    for (i, m) in obj.iter().enumerate() {
-        let mesh = &m.mesh;
-
-        println!("model[{}].name = \'{}\'", i, m.name);
-        println!("model[{}].mesh.material_id = {:?}", i, mesh.material_id);
-
-        println!(
-            "Size of model[{}].face_arities: {}",
-            i,
-            mesh.face_arities.len()
-        );
-
-        let mut next_face = 0;
-        for f in 0..mesh.face_arities.len() {
-            let end = next_face + mesh.face_arities[f] as usize;
-            let face_indices: Vec<_> = mesh.indices[next_face..end].iter().collect();
-            println!("    face[{}] = {:?}", f, face_indices);
-            next_face = end;
-        }
-
-        // Normals and texture coordinates are also loaded, but not printed in this example
-        println!("model[{}].vertices: {}", i, mesh.positions.len() / 3);
-
-        assert_eq!(mesh.positions.len() % 3, 0);
-        for v in 0..mesh.positions.len() / 3 {
-            println!(
-                "    v[{}] = ({}, {}, {})",
-                v,
-                mesh.positions[3 * v],
-                mesh.positions[3 * v + 1],
-                mesh.positions[3 * v + 2]
-            );
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
 
